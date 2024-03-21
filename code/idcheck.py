fd3a0e212053768deda472eb690bfbdb664a73fc
cyberraya7 <123887753+cyberraya7@users.noreply.github.com>
2024-03-22 00:33:38 +0800

    Update idcheck.py

--- code/idcheck.py
+++ code/idcheck.py
@@ -265,7 +265,6 @@ if selected == "About":
         ### Check Credential
         - The largest dataset of leaked credentials, COMB (Combination Of Many Breaches) containing over billions of usernames and passwords from various breaches, was publicly disclosed in 2021.
         - This tool enables easy searching of this massive dataset, allowing individuals to check if their credentials were exposed and promoting enhanced security practices. It emphasizes the importance of changing passwords promptly and enabling two-factor authentication when available. 
-        - No search records are logged or stored on the servers. 
         - Datasource: https://www.proxynova.com/
         ### Check Email
         - Check if your email have been compromised on any data breaches.
@@ -273,7 +272,6 @@ if selected == "About":
         ### Check Password
         - Consist of hundreds of millions of real-world passwords that have been compromised in data breaches. 
         - Due to this exposure, using them for ongoing purposes poses a significantly higher risk of being exploited to compromise other accounts.
-        - No search records are logged or stored on the servers. 
         
     """
     )
